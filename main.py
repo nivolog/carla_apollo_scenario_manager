ae7a22a0f3df06cf1eaab16cf40d37547aeb117d
nivolog <vlad.golovin.orenburg@yandex.ru>
2023-01-26 05:47:45 +0300

    Added scenario run. Config changes

--- main.py
+++ main.py
@@ -5,21 +5,36 @@ from scenario import Scenario
 
 def run_scenario(**kwargs): # TODO: add scenario creation, check parameters
     # We create scenarion and starts it
-    #scenario = Scenario()
-    pass
+    scenario = Scenario(**kwargs)
+    if not scenario.load_scenario():
+        return False
+    if not scenario.prepare_scenario():
+        return False
+    
+    while True:
+        if not scenario.sim_loop():
+            break
     
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', default=None, type=str,
-                        help='Path to config file')
+                        help='Path to config file', required=True)
+    parser.add_argument('--scenario', default=None, type=str,
+                        help='[Optional] Scenario to choose. Overrides scenario in config file', required=False)
     args = parser.parse_args()
-    if args.config:
-        config_file = ET.parse(args.config)
-        kwargs = {}
-        for item in config_file.getroot():
-            kwargs[item.tag] = item.text
-        run_scenario(**kwargs)
-    else:
+    
+    if args.config is None:
         print('No config file provided!')
         pass
+    
+    config_file = ET.parse(args.config)
+    kwargs = {}
+    for item in config_file.getroot():
+        kwargs[item.tag] = item.text
+    
+    if args.scenario is not None:
+        kwargs['scenario'] = args.scenario
+    
+    if run_scenario(**kwargs):
+        print('Success!')
